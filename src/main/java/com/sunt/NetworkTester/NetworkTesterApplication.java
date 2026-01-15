f781cb65fc7c017f8edd5fd63d071b9d0497fa53
AdrienBref <adrianbernabeufrutos@hotmail.com>
2026-01-15 12:44:06 +0100

    test jenkins with Poll Scm

--- src/main/java/com/sunt/NetworkTester/NetworkTesterApplication.java
+++ src/main/java/com/sunt/NetworkTester/NetworkTesterApplication.java
@@ -11,7 +11,7 @@ public class NetworkTesterApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(NetworkTesterApplication.class, args);
-		System.out.println("Hola mundo!");	
+
 	}
 
 }
